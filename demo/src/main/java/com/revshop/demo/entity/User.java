8bfcfa460565cb6d12aabd8a785b4dd968e5d875
bbhakta097 <bbhakta097@hotmail.com>
2025-01-28 14:15:24 -0600

    Committed login and Register

--- demo/src/main/java/com/revshop/demo/entity/User.java
+++ demo/src/main/java/com/revshop/demo/entity/User.java
@@ -4,9 +4,22 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchConnectionDetails;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 @Data
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "users")
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = Buyer.class, name = "buyer"),
+    @JsonSubTypes.Type(value = Seller.class, name = "seller")
+})
 public abstract class User {
 
     @Id
@@ -32,6 +45,18 @@ public abstract class User {
     public enum Role {
         BUYER,
         SELLER
+    }
+
+    public User(){}
+
+    public User(Long userId, String email, String firstName, String lastName, String password, Role role, String username) {
+        this.id = userId;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.role = role;
+        this.username = username;
     }
 
 }
