2e92a5abd1b722ac7c76d8c8ea880b24308dbae7
Matt <84721683+mattry@users.noreply.github.com>
2025-02-03 11:38:50 -0500

    Update context to set local storage information for some browser persistance

--- frontend/src/components/UserContext.jsx
+++ frontend/src/components/UserContext.jsx
@@ -1,18 +1,30 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
 const UserContext = createContext();
 
 export const UserProvider = ({children}) => {
 
-    const [user, setUser] = useState(null);
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+
+    const [user, setUser] = useState(storedUser || null);
 
     const updateUser = (newUser) => {
         setUser(newUser);  
+        localStorage.setItem("user", JSON.stringify(newUser));
     }
 
     const clearUser = () => {
         setUser(null);
+        localStorage.removeItem("user");
     }
+
+    useEffect(() => {
+        if (user) {
+            localStorage.setItem("user", JSON.stringify(user));
+        } else {
+            localStorage.removeItem("user");
+        }
+    }, [user]);
 
     return (
         <UserContext.Provider value ={{ user, updateUser, clearUser }}>
