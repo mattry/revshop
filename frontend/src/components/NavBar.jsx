6fdac0c2e7c7ac6fa43d4ae46d819150048e4938
Matt <84721683+mattry@users.noreply.github.com>
2025-02-03 09:07:42 -0500

    Update NavBar Component to use Material UI's AppBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,20 +1,32 @@
 import { Link } from "react-router-dom";
 import { useUser } from "./UserContext";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 
 const NavBar = () => {
 
     const { user } = useUser();
 
     return (
-        <>
-            {user ?
-                <>
-                    <Link to="/">Home</Link>
-                    <Link to="inventory">Inventory</Link>
-                </>
-            : null }
-        </>
-    )
+        <Box sx={{ m: 0, p: 0}}>
+            <AppBar position="static">
+                <Toolbar sx={{ px: 2}}>
+                    <Typography variant="h6" sx={{ flexGrow: 1, ml: 0}}>
+                        RevShop
+                    </Typography>
+                {user && (
+                    <>
+                        <Button color="inherit" component={Link} to="/">
+                            Home
+                        </Button>
+                        <Button color="inherit" component={Link} to="/inventory">
+                            Inventory
+                        </Button>
+                    </>
+                )}
+                </Toolbar>
+            </AppBar>
+      </Box>
+    );
 
 }
 
