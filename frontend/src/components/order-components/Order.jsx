a1a0c8f7f084c79b422089a15fdca702848577e6
Matt <84721683+mattry@users.noreply.github.com>
2025-02-07 09:38:41 -0500

    Update Order component to display order details

--- frontend/src/components/order-components/Order.jsx
+++ frontend/src/components/order-components/Order.jsx
@@ -1,7 +1,31 @@
 const Order = ({ order }) => {
+    const items = order.items;
+
     return(
         <>
             <h4>Order No. {order.orderId}</h4>
+            <h5>Order Details:</h5>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Price per Unit</th>
+                        <th>Quantity</th>
+                        <th>Subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map((item) => (
+                        <tr key={item.productId}>
+                            <td>{item.productName}</td>
+                            <td>${item.price.toFixed(2)}</td>
+                            <td>{item.quantity}</td>
+                            <td>${item.subTotal.toFixed(2)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <h6>Total: ${order.total.toFixed(2)}</h6>
         </>
     )
 }
